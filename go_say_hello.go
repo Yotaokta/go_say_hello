ef045eb59d84c5a15a5621d800af54e2e3bd4f38
Muhammad Yota <muhammadyota86@gmail.com>
2024-04-02 04:21:53 +0700

    update pertama

--- go_say_hello.go
+++ go_say_hello.go
@@ -1,9 +1,9 @@
 package go_say_hello
 
 func SayHello(nama string) string {
-	return "hello" + nama
+	return "hello " + nama
 }
 
 func ConfirmMessage(nama string) string {
-	return SayHello(nama) + "Mau Pesan Apa?"
+	return SayHello(nama) + " Mau Pesan Apa?"
 }
